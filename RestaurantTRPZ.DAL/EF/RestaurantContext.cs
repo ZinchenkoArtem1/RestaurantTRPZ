9888a51d6094dab012fe00bc543d5439c4140f71
Artem Zinchenko <promakey76@gmail.com>
2021-02-18 18:04:06 +0200

    Add UnitOfWork pattern and create RestaurantContext(Mock of database)

---
+++ RestaurantTRPZ.DAL/EF/RestaurantContext.cs
@@ -0,0 +1,27 @@
+﻿using RestaurantTRPZ.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantTRPZ.DAL.EF
+{
+    public class RestaurantContext
+    {
+        ICollection<Dish> Dishes { get; set; }
+        ICollection<Order> Orders { get; set; }
+        ICollection<Equipment> Equipments { get; set; }
+        ICollection<Cook> Cooks { get; set; }
+        ICollection<Ingredient> Ingredients { get; set; }
+
+        public RestaurantContext()
+        {
+            Dishes = new HashSet<Dish>();
+            Orders = new HashSet<Order>();
+            Equipments = new HashSet<Equipment>();
+            Cooks = new HashSet<Cook>();
+            Ingredients = new HashSet<Ingredient>();
+        }
+    }
+}
---
+++ RestaurantTRPZ.DAL/Repositories/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using RestaurantTRPZ.DAL.EF;
+using RestaurantTRPZ.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantTRPZ.DAL.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly RestaurantContext _context;
+
+        public ICookRepository Cooks { get; }
+        public IDishRepository Dishes { get; }
+        public IOrderRepository Orders { get; }
+
+        public UnitOfWork(RestaurantContext context, ICookRepository cookRepository,
+            IDishRepository dishRepository, IOrderRepository orderRepository)
+        {
+            _context = context;
+            Cooks = cookRepository;
+            Dishes = dishRepository;
+            Orders = orderRepository;
+        }
+
+        public void Save()
+        {
+            // _context.SaveChanging() when add EF to project
+        }
+    }
+}
